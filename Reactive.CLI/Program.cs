0445be7905f792b81af6f37df90a4f295e0c83ac
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-07-27 17:18:23 -0300

    feat: Buscar usuários

--- Reactive.CLI/Program.cs
+++ Reactive.CLI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Reactive.CLI.Connectors;
@@ -14,6 +15,12 @@ namespace Reactive.CLI
             Console.WriteLine("Creating API Services");
             var api = RestService.For<ITodoApi>("https://jsonplaceholder.typicode.com/");
             Console.WriteLine("API Created for JsonPlaceholder");
+            var users = await api.ListUsers();
+            Console.WriteLine("Recovered {0} users from the API", users.Count());
+            foreach (var user in users)
+            {
+                Console.WriteLine("{0} works to {1}", user.Name, user.Company.Name);
+            }
         }
     }
 }
