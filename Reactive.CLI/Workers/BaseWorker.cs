b7b9fced3367f541bffdcfe720f857efb7cd2ff2
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-07-30 20:36:30 -0300

    feat: Abstrair Worker base

---
+++ Reactive.CLI/Workers/BaseWorker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Reactive.CLI.Connectors;
+
+namespace Reactive.CLI.Workers
+{
+    public abstract class BaseWorker
+    {
+        protected ITodoApi Api { get; private set; }
+
+        protected BaseWorker(ITodoApi api)
+        {
+            Api = api ?? throw new ArgumentNullException(nameof(api));
+        }
+
+        protected Task Wait() => Task.Delay(1000 * 60);
+    }
+}
---
+++ Reactive.CLI/Workers/IWorker.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reactive.CLI.Workers
+{
+    public interface IWorker
+    {
+        Task DoWork(CancellationToken cancellationToken = default);
+    }
+}
