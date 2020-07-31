506cc5b6f7cd755db567997ba8fc1a744f3c32d4
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-07-30 22:50:41 -0300

    feat: Criar worker para Posts

---
+++ Reactive.CLI/Workers/PostsWorker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+using Reactive.CLI.Connectors;
+
+namespace Reactive.CLI.Workers
+{
+    public class PostsWorker : BaseWorker, IWorker<Post>
+    {
+        private BehaviorSubject<Post> _post = new BehaviorSubject<Post>(new Post { });
+
+        public PostsWorker(ITodoApi api) : base(api)
+        {
+        }
+
+        public IObservable<Post> State => _post;
+
+        public IDisposable Run() => Observable
+            .Interval(TimeSpan.FromSeconds(30))
+            .Timestamp()
+            .Subscribe(async l =>
+            {
+                Console.WriteLine("{0} Fetching posts from API", l.Timestamp.ToLocalTime());
+                var posts = await Api.ListPosts();
+                foreach (var post in posts)
+                {
+                    _post.OnNext(post);
+                }
+            });
+    }
+}
